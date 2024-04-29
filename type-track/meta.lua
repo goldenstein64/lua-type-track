1418f22c44c267a5b9f35c6c2d881178cc23f7f5
goldenstein64 <goldnstein64@gmail.com>
2024-04-28 23:39:41 -0400

    feat: Inheritable has a class def syntax

--- type-track/meta.lua
+++ type-track/meta.lua
@@ -6,9 +6,14 @@ local muun = require("type-track.muun")
 ---@field __class any
 local Inheritable = muun("Inheritable")
 
----@param cls any
+---@param self unknown
+---@param cls unknown
 ---@return boolean
-function Inheritable:is_instance(cls)
+function Inheritable.is_instance(self, cls)
+	if type(self) ~= "table" or not cls then
+		return false
+	end
+
 	local obj_cls = self.__class
 	while obj_cls and obj_cls ~= cls do
 		obj_cls = obj_cls.__parent
